49f1b9f562b881a7bc0866ab15875b41bc96020d
Apriyantiii <nartiapriyanti@gmail.com>
2024-01-13 22:18:45 +0700

    landing-page done, minus logo yang ada di atas

--- resources/views/layouts/landing-page/footer.blade.php
+++ resources/views/layouts/landing-page/footer.blade.php
@@ -1,28 +1,51 @@
-<footer class="footer fixed-bottom">
-    {{-- <div class="row">
-        <div class="col-sm-6">
-            <script>
-                document.write(new Date().getFullYear())
-            </script> © Dason.
-        </div>
-        <div class="col-sm-6">
-            <div class="text-sm-end d-none d-sm-block">
-                Design & Develop by <a href="#!" class="text-decoration-underline">Themesdesign</a>
+{{-- Footer Start --}}
+<footer class="ftco-footer ftco-section">
+    <div class="container-fluid px-md-5">
+        <div class="row mb-5">
+            <div class="col-md">
+                <div class="ftco-footer-widget mb-4">
+                    <h1 class="ftco-heading-2"><strong>PT. Mutiara Putri Gemilang</strong></h1>
+                    <h5 class="text-secondary">Properti Impian, Investasi Masadepan</h5>
+                    <ul class="ftco-footer-social list-unstyled mt-5">
+                        <li class="ftco-animate"><a href="#"><span class="icon-twitter"></span></a></li>
+                        <li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
+                        <li class="ftco-animate"><a href="#"><span class="icon-instagram"></span></a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-md">
+                <div class="ftco-footer-widget mb-4">
+                    <h1 class="ftco-heading-2"><strong>Ada yang Perlu Ditanyakan?</strong></h1>
+                    <div class="block-23 mt-3 mb-3">
+                        <h6 class="text-secondary mb-3"><i class="fas fa-map-marker-alt me-2"></i><span class="text">
+                                Jalan Merdeka,
+                                Desa/Kelurahan Gondek, Kec. Mojowarno, Kab.
+                                Jombang, Provinsi Jawa Timur,
+                                Kode Pos: 61475
+                            </span></h6>
+
+                        <h6 class="text-secondary mb-3"><a href="#"><i class="fas fa-phone me-2"></i><span
+                                    class="text">+62-812-4998-5217</span></a></h6>
+
+                        <h6 class="text-secondary"><a href="#"><i class="fas fa-envelope me-2"></i><span
+                                    class="text">pt.mutiaraputrigemilang@gmail.com</span></a></h6>
+                    </div>
+                </div>
             </div>
         </div>
-    </div> --}}
-
-    <div class="row">
-        <div class="col-md-12 text-center">
-
-            <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-                Copyright &copy;
-                <script>
-                    document.write(new Date().getFullYear());
-                </script> PT. Mutiara Putri Gemilang <i class="icon-heart color-danger"
-                    aria-hidden="true"></i> by Apriyanti
-                <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-            </p>
+        <div class="row">
+            <div class="col-md-12 text-center">
+                <h6 class="text-secondary">
+                    <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                    Copyright &copy;
+                    <script>
+                        document.write(new Date().getFullYear());
+                    </script> PT. Mutiara Putri Gemilang <i class="icon-heart color-danger"
+                        aria-hidden="true"></i> by Apriyanti
+                    <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                </h6>
+            </div>
         </div>
     </div>
 </footer>
+{{-- Footer End --}}
--- resources/views/layouts/landing-page/master.blade.php
+++ resources/views/layouts/landing-page/master.blade.php
@@ -3,150 +3,71 @@
 
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>E-Kavling PT.MGP</title>
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
+    <meta content="Themesdesign" name="author" />
+    <!-- App favicon -->
+    <link rel="shortcut icon" href="{{ URL::asset('assets/images/favicon.ico') }}">
 
-    <title>E-Kavling PT.MPG</title>
-
-    <!-- Fonts -->
-    {{-- <link rel="dns-prefetch" href="//fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet"> --}}
-
+    <!-- Your Internal CSS -->
+    <style>
+        .navbar-nav .nav-item:hover .nav-link,
+        .navbar-nav .nav-item.active .nav-link {
+            border-bottom: 2px solid #4b69bd;
+        }
+    </style>
     @include('layouts.head-css')
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-nlQBWqkn9f5JPDl5XVz0NpCqeFLZ5fmbo5PXt22GHdLlB4iEJ2fUKPJz/G9CpeJ1" crossorigin="anonymous">
-    </script>
-    <!-- Scripts -->
-    {{-- @vite(['resources/sass/app.scss', 'resources/js/app.js']) --}}
 </head>
 
-<body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow fixed-top">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    <span class="logo-lg">
-                        <img src="{{ URL::asset('assets/images/logo-e-kavling.png') }}" alt="" height="50">
-                        <span class="logo-txt">PT. MGP</span>
-                </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                    aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+@section('body')
+    @include('layouts.body')
+@show
+<div id="app">
+    {{-- navbar Start --}}
+    @include('layouts.landing-page.navbar')
+    {{-- navbar End --}}
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav me-auto">
+    <main class="py-0 mb-5">
+        @yield('landing-page')
+    </main>
 
-                    </ul>
+    {{-- Footer Start --}}
+    @include('layouts.landing-page.footer')
+    {{-- Footer End --}}
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ms-auto">
-                        <li class="nav-item dropdown">
-                        <li class="nav-item fs-5"><a href="#home-section" class="nav-link"><span>Beranda</span></a></li>
-                        {{-- <li class="nav-item"><a href="#services-section" class="nav-link"><span>Visi Misi</span></a></li> --}}
-                        <li class="nav-item"><a href="#properti-unggulan"
-                                class="nav-link fs-5"><span>Properti</span></a>
-                        </li>
+</div>
 
-                        <li class="nav-item dropdown">
-                            <a class="nav-link fs-5 dropdown-toggle" data-toggle="dropdown" href="#"
-                                role="button" aria-expanded="false">Tentang</a>
-                            <div class="dropdown-menu">
-                                <a class="dropdown-item" href="#visi-misi">Visi Misi</a>
-                                <a class="dropdown-item" href="#about-section">Tentang Perusahaan</a>
-                                <a class="dropdown-item" href="#komisaris">Dewan Komisaris</a>
-                                <a class="dropdown-item" href="#prosedur-pembelian">Prosedur Pembelian</a>
-                            </div>
-                        </li>
-                        {{-- <li class="nav-item"><a href="#prosedur-pembelian" class="nav-link"><span>Prosedur Pembelian</span></a></li> --}}
-                        {{-- <li class="nav-item"><a href="#agent-section" class="nav-link"><span>Dewan Komisaris</span></a></li> --}}
-                        {{-- <li class="nav-item"><a href="#blog-section" class="nav-link"><span>Blog</span></a></li> --}}
-                        <li class="nav-item"><a href="#testimoni" class="nav-link fs-5"><span>Testimoni</span></a></li>
-                        {{-- <li class="nav-item"><a href="#contact-section" class="nav-link"><span>Kontak</span></a></li> --}}
+<!-- JAVASCRIPT -->
+<script>
+    document.addEventListener('DOMContentLoaded', function() {
+        window.addEventListener('hashchange', function() {
+            updateActiveTab();
+        });
 
-                        @if (Route::has('login'))
-                            @auth
-                                <li class="nav-item"><a href="{{ url('/home') }}"
-                                        class="nav-link fs-5"><span>Home</span></a></li>
-                            @else
-                                <li class="nav-item"><a href="{{ route('login') }}" class="nav-link fs-5"><span>Log
-                                            in</span></a></li>
+        updateActiveTab();
+    });
 
-                                @if (Route::has('register'))
-                                    <li class="nav-item"><a href="{{ route('register') }}"
-                                            class="nav-link fs-5"><span>Register</span></a></li>
-                                @endif
-                            @endauth
+    function updateActiveTab() {
+        var hash = window.location.hash;
 
-                        @endif
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
+        var navbarItems = document.querySelectorAll('.navbar-nav .nav-item');
 
-        <main class="py-0 mb-5">
-            @yield('landing-page')
-        </main>
+        navbarItems.forEach(function(item) {
+            item.classList.remove('active');
+        });
 
-        {{-- Footer Start --}}
-        <footer class="ftco-footer ftco-section">
-            <div class="container-fluid px-md-5">
-                <div class="row mb-5">
-                    <div class="col-md">
-                        <div class="ftco-footer-widget mb-4">
-                            <h1 class="ftco-heading-2"><strong>PT. Mutiara Putri Gemilang</strong></h1>
-                            <h5 class="text-secondary">Properti Impian, Investasi Masadepan</h5>
-                            <ul class="ftco-footer-social list-unstyled mt-5">
-                                <li class="ftco-animate"><a href="#"><span class="icon-twitter"></span></a></li>
-                                <li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
-                                <li class="ftco-animate"><a href="#"><span class="icon-instagram"></span></a></li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="col-md">
-                        <div class="ftco-footer-widget mb-4">
-                            <h1 class="ftco-heading-2"><strong>Ada yang Perlu Ditanyakan?</strong></h1>
-                            <div class="block-23 mt-3 mb-3">
-                                <h6 class="text-secondary mb-3"><i class="fas fa-map-marker-alt me-2"></i><span
-                                        class="text">
-                                        Jalan Merdeka,
-                                        Desa/Kelurahan Gondek, Kec. Mojowarno, Kab.
-                                        Jombang, Provinsi Jawa Timur,
-                                        Kode Pos: 61475
-                                    </span></h6>
+        var activeLink = document.querySelector('.navbar-nav .nav-item a[href="' + hash + '"]');
+        if (activeLink) {
+            activeLink.parentElement.classList.add('active');
+        }
+    }
+</script>
 
-                                <h6 class="text-secondary mb-3"><a href="#"><i class="fas fa-phone me-2"></i><span
-                                            class="text">+62-812-4998-5217</span></a></h6>
 
-                                <h6 class="text-secondary"><a href="#"><i class="fas fa-envelope me-2"></i><span
-                                            class="text">pt.mutiaraputrigemilang@gmail.com</span></a></h6>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-md-12 text-center">
-                        <h6 class="text-secondary">
-                            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-                            Copyright &copy;
-                            <script>
-                                document.write(new Date().getFullYear());
-                            </script> PT. Mutiara Putri Gemilang <i class="icon-heart color-danger"
-                                aria-hidden="true"></i> by Apriyanti
-                            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-                        </h6>
-                    </div>
-                </div>
-            </div>
-        </footer>
-        {{-- Footer End --}}
-    </div>
+
+@include('layouts.vendor-scripts')
 
 
 </body>
